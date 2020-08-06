d403f5eb56a89388dc015c9dfde1c653258cc098
Daniel Marifjeren <marifjeren1@yahoo.com>
2020-08-06 18:48:06 -0400

    Added palandrome functionality
    Added Paragraph, sentence and word class.
    
    Paragraph now checks to see how many sentences are palanadrums and how many words are paladrones.
    
    Next step is to add display for both sentence and word palandrome counts.

--- PalandromeAssignment/PalandromeAssignment/Form1.cs
+++ PalandromeAssignment/PalandromeAssignment/Form1.cs
@@ -12,9 +12,31 @@ namespace PalandromeAssignment
 {
     public partial class Form1 : Form
     {
+        public string paragraphText; 
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void submit_Btn_Click(object sender, EventArgs e)
+        {
+            paragraphText = paragraph_TB.Text;
+
+            initParagraph();
+        }
+
+        void initParagraph()
+        {
+            Paragraph paragraph = new Paragraph();
+            if (paragraphText != null)
+            {
+                paragraph_Label.Text = paragraphText; 
+                paragraph.developTheParagraph(paragraphText);
+            }
+            else
+                paragraph_Label.Text = "Please enter a paragraph";
+            /*split words*/
         }
     }
 }
